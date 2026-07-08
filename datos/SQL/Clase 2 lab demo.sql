1aeeedbd17b0f3870503c6f9428082716e17dadc
marianunezp <maria.nunez.p@usach.cl>
2026-07-08 17:43:06 -0400

    clase 9 de julio vuelta paro

--- datos/SQL/Clase 2 lab demo.sql
+++ datos/SQL/Clase 2 lab demo.sql
@@ -46,8 +46,8 @@ ORDER BY "edad_personas_mayores"
 
 SELECT c."nom_comuna", 
 COUNT(*) AS total_personas,
-COUNT(*) FILTER( WHERE P.P09>=65) AS personas_mayores,
-ROUND ((COUNT(*) FILTER( WHERE P.P09>=65))*100.0/(COUNT(*)),4) AS ptje_personas_mayores
+COUNT(*) FILTER( WHERE P.P09>=65) AS edad_personas_mayores,
+(COUNT(*))*100/(COUNT(*) FILTER( WHERE P.P09>=65))
 FROM personas AS p
 JOIN hogares AS h
 ON p."hogar_ref_id"= h."hogar_ref_id"
@@ -58,43 +58,6 @@ ON z."zonaloc_ref_id" = v."zonaloc_ref_id"
 JOIN comunas AS c
 ON c."codigo_comuna"= z."codigo_comuna"
 GROUP BY c."nom_comuna"
-ORDER BY "personas_mayores" DESC
+ORDER BY "edad_personas_mayores"
 
----------------------------------------------------------------------------------------
---clase 3 geodemo
------cantidad de personas mayores de edad por zona censal
-SELECT z.geocodigo,
-		c.nom_comuna, 
-COUNT(*) AS total_personas,
-COUNT(*) FILTER( WHERE P.P09>=65) AS personas_mayores,
-ROUND ((COUNT(*) FILTER( WHERE P.P09>=65))*100.0/(COUNT(*)),4) AS ptje_personas_mayores
-FROM personas AS p
-JOIN hogares AS h
-ON p."hogar_ref_id"= h."hogar_ref_id"
-JOIN viviendas AS v
-ON h."vivienda_ref_id" = v."vivienda_ref_id"
-JOIN zonas AS z
-ON z."zonaloc_ref_id" = v."zonaloc_ref_id"
-JOIN comunas AS c
-ON c."codigo_comuna"= z."codigo_comuna"
-GROUP BY z.geocodigo, c.nom_comuna
-ORDER BY "personas_mayores" DESC
 
------------------------ 
---- cantidad de profesionales por zona censal
-SELECT z.geocodigo,
-		c.nom_comuna, 
-COUNT(*) AS total_personas,
-COUNT(*) FILTER( WHERE P.P15>=12 and P.P15<=14) AS total_profesionales,
-ROUND ((COUNT(*) FILTER( WHERE P.P15>=12 and P.P15<=14))*100.0/(COUNT(*)),4) AS ptje_profesionales
-FROM personas AS p
-JOIN hogares AS h
-ON p."hogar_ref_id"= h."hogar_ref_id"
-JOIN viviendas AS v
-ON h."vivienda_ref_id" = v."vivienda_ref_id"
-JOIN zonas AS z
-ON z."zonaloc_ref_id" = v."zonaloc_ref_id"
-JOIN comunas AS c
-ON c."codigo_comuna"= z."codigo_comuna"
-GROUP BY z.geocodigo, c.nom_comuna
-ORDER BY "total_profesionales" DESC
